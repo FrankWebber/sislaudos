ee0579ea31ad4596e41e092892d881c84dd32dae
Williams <cdrmw16@gmail.com>
2024-11-12 18:12:25 -0400

    Update textCorrector.js

--- public/JS/textCorrector.js
+++ public/JS/textCorrector.js
@@ -4,8 +4,11 @@ function text_cleaner() {
 
     // Expressão regular para encontrar a matrícula, com ou sem formatação
     text = text.replace(/(matr[íi]cula\s*n[ºo°]?\s*[^\d]*)(\d{3})\.?(\d{3})-?(\d)([A-Z]?)/gi, function (match, prefix, part1, part2, part3, letter) {
-        // Formata a matrícula no padrão 000.000-0A
-        return `${prefix}${part1}.${part2}-${part3}${letter || ''}`;
+        // Se o último caractere (letra) estiver ausente ou for '0', substitui por 'C'
+        letter = (!letter || letter === '0') ? 'C' : letter;
+
+        // Formata a matrícula no padrão 000.000-0A (garantindo que o último caractere seja uma letra)
+        return `${prefix}${part1}.${part2}-${part3}${letter}`;
     });
 
     // Atualiza o conteúdo do textarea com o texto formatado
